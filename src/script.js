716715178d849bb731e901b3e54096e9fdb11bfd
kanitmann <kanitmann90@gmail.com>
2022-01-21 00:40:47 +0530

    Demo Script added Theory Slots added

---
+++ src/script.js
@@ -0,0 +1,511 @@
+let text = `1
+	
+
+General (Semester)
+	
+
+CSE1901 - Technical Answers for Real World Problems (TARP)
+
+( Embedded Theory )
+	
+
+1 0 0 0 1.0
+	University Core 	
+
+Regular
+	
+
+VL2021220503466
+	
+
+TG1 -
+
+SJT505
+	
+
+AMUTHA PRABAKAR M -
+
+SCOPE
+	
+
+04-Dec-2021 14:11
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+2
+	
+
+General (Semester)
+	
+
+CSE1901 - Technical Answers for Real World Problems (TARP)
+
+( Embedded Project )
+	
+
+0 0 0 4 1.0
+	University Core 	
+
+Regular
+	
+
+VL2021220503467
+	
+
+TG1 -
+
+NIL
+	
+
+AMUTHA PRABAKAR M -
+
+SCOPE
+	
+
+04-Dec-2021 14:11
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+3
+	
+
+General (Semester)
+	
+
+CSE3002 - Internet and Web Programming
+
+( Embedded Theory )
+	
+
+2 0 0 0 2.0
+	Programme Core 	
+
+Regular
+	
+
+VL2021220503380
+	
+
+B2 -
+
+SJT224
+	
+
+SRIDHAR RAJ S -
+
+SCOPE
+	
+
+04-Dec-2021 14:09
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+4
+	
+
+General (Semester)
+	
+
+CSE3002 - Internet and Web Programming
+
+( Embedded Lab )
+	
+
+0 0 2 0 1.0
+	Programme Core 	
+
+Regular
+	
+
+VL2021220503387
+	
+
+L27+L28 -
+
+SJT517
+	
+
+SRIDHAR RAJ S -
+
+SCOPE
+	
+
+04-Dec-2021 14:09
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+5
+	
+
+General (Semester)
+	
+
+CSE3002 - Internet and Web Programming
+
+( Embedded Project )
+	
+
+0 0 0 4 1.0
+	Programme Core 	
+
+Regular
+	
+
+VL2021220503388
+	
+
+L27+L28 -
+
+NIL
+	
+
+SRIDHAR RAJ S -
+
+SCOPE
+	
+
+04-Dec-2021 14:09
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+6
+	
+
+General (Semester)
+	
+
+CSE3050 - Data Visualization and Presentation
+
+( Embedded Theory )
+	
+
+3 0 0 0 3.0
+	University Elective 	
+
+Regular
+	
+
+VL2021220502580
+	
+
+C1+TC1 -
+
+MB311
+	
+
+VISHNU SRINIVASA MURTHY Y -
+
+SCOPE
+	
+
+11-Dec-2021 14:11
+	
+
+12-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+7
+	
+
+General (Semester)
+	
+
+CSE3050 - Data Visualization and Presentation
+
+( Embedded Lab )
+	
+
+0 0 2 0 1.0
+	University Elective 	
+
+Regular
+	
+
+VL2021220502599
+	
+
+L43+L44 -
+
+SJT416
+	
+
+VISHNU SRINIVASA MURTHY Y -
+
+SCOPE
+	
+
+11-Dec-2021 14:11
+	
+
+12-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+8
+	
+
+General (Semester)
+	
+
+CSE3502 - Information Security Management
+
+( Embedded Theory )
+	
+
+2 0 0 0 2.0
+	Programme Elective 	
+
+Regular
+	
+
+VL2021220502523
+	
+
+F2 -
+
+SJT422
+	
+
+VIMALA DEVI K -
+
+SCOPE
+	
+
+04-Dec-2021 14:06
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+9
+	
+
+General (Semester)
+	
+
+CSE3502 - Information Security Management
+
+( Embedded Lab )
+	
+
+0 0 2 0 1.0
+	Programme Elective 	
+
+Regular
+	
+
+VL2021220502642
+	
+
+L7+L8 -
+
+SJT418
+	
+
+VIMALA DEVI K -
+
+SCOPE
+	
+
+04-Dec-2021 14:06
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+10
+	
+
+General (Semester)
+	
+
+CSE3502 - Information Security Management
+
+( Embedded Project )
+	
+
+0 0 0 4 1.0
+	Programme Elective 	
+
+Regular
+	
+
+VL2021220502643
+	
+
+L7+L8 -
+
+NIL
+	
+
+VIMALA DEVI K -
+
+SCOPE
+	
+
+04-Dec-2021 14:06
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+11
+	
+
+General (Semester)
+	
+
+STS3104 - Enhancing Programming Ability
+
+( Soft Skill )
+	
+
+3 0 0 0 1.0
+	University Core (GENERAL Basket) 	
+
+Regular
+	
+
+VL2021220500149
+	
+
+D1+TD1 -
+
+SJT311
+	
+
+FACE (APT) -
+
+SSL
+	
+
+04-Dec-2021 14:03
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved`;
+
+// Theory Slots
+
+let A1 = [[0, 0800, 0850], [2, 0900, 0950]];
+let B1 = [[1, 0800, 0850], [3, 0900, 0950]];
+let C1 = [[2, 0800, 0850], [4, 0900, 0950]];
+let D1 = [[3, 0800, 0850], [0, 0900, 0950]];
+let E1 = [[4, 0800, 0850], [1, 0900, 0950]];
+let F1 = [[0, 0900, 0950], [2, 1000, 1050]];
+let G1 = [[1, 0900, 0950], [3, 1000, 1050]];
+
+let TA1 = [4, 1000, 1050];
+let TB1 = [0, 1100, 1150];
+let TC1 = [1, 1100, 1150];
+let TD1 = [4, 1200, 1250];
+let TE1 = [3, 1100, 1150];
+let TF1 = [4, 1100, 1150];
+let TG1 = [0, 1200, 1250];
+let TAA1 = [1, 1200, 1250];
+let TCC1 = [3, 1200, 1250];
+
+let mrng_slots = [A1, B1, C1, D1, E1, F1, G1];
+let single_slots1 = [TA1, TB1, TC1, TD1, TE1, TF1, TG1, TAA1];
+let V1 = [2, 1100, 1150];
+let V2 = [2, 1200, 1250];
+
+let A2 = A1, B2 = B1, C2 = C1, D2 = D1, E2 = E1, F2 = F1, G2 = G1, TA2 = TA1, TB2 = TB1, TC2 = TC1, TD2 = TD1, TE2 = TE1, TF2 = TF1, TG2 = TG1;
+let aftr_slots = [A2, B2, C2, D2, E2, F2, G2];
+let TAA2 = [1, 1800, 1850];
+let TBB2 = [2, 1800, 1850];
+let TDD2 = [4, 1800, 1850];
+let single_slots2 = [TA2, TB2, TC2, TD2, TE2, TF2, TG2];
+
+for (let i = 0; i < mrng_slots.length; i++) {
+    for (let j = 0; j < 2; j++) {
+
+        aftr_slots[i][j][1] = mrng_slots[i][j][1] + 600;
+        aftr_slots[i][j][2] = mrng_slots[i][j][2] + 600;
+
+    }
+    single_slots2[i][1] = single_slots1[i][1] + 600;
+    single_slots2[i][2] = single_slots1[i][2] + 600;
+}
+
+let theory = [A1, A2, B1, B2, C1, C2, D1, D2, E1, E2, F1, F2, G1, G2, TA1, TA2, TB1, TB2, TC1, TC2, TD1, TD2, TE1, TE2, TF1, TF2, TG1, TG2, TAA1, TAA2, TBB2, TCC1, TDD2, V1, V2];
+console.log(theory);
+
+// Lab Slots
+
+let L1 = [0, 0800, 0850];
+let L2 = [0, 0851, 0940];
+let L3 = [0, 0951, 1040];
+let L4 = [0, 1041, 1130];
+let L5 = [0, 1140, 1230];
+let L6 = [0, 1231, 1320];
+
+let L7 = [1, 0800, 0850];
+let L8 = [1, 0851, 0940];
+let L9 = [1, 0951, 1040];
+let L10 = [1, 1041, 1130];
+let L11 = [1, 1140, 1230];
+let L12 = [1, 1231, 1320];
+
+let L13 = [2, 0800, 0850];
+let L14 = [2, 0851, 0940];
+let L15 = [2, 0951, 1040];
+let L16 = [2, 1041, 1130];
+let L17 = [2, 1140, 1230];
+let L18 = [2, 1231, 1320];
+
+let L19 = [3, 0800, 0850];
+let L20 = [3, 0851, 0940];
+let L21 = [3, 0951, 1040];
+let L22 = [3, 1041, 1130];
+let L23 = [3, 1140, 1230];
+let L24 = [3, 1231, 1320];
+
+let L25 = [4, 0800, 0850];
+let L26 = [4, 0851, 0940];
+let L27 = [4, 0951, 1040];
+let L28 = [4, 1041, 1130];
+let L29 = [4, 1140, 1230];
+let L30 = [4, 1231, 1320];
