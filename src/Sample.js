5c525e2136b90da8d290ad41102a92da60261efa
kanitmann <kanitmann90@gmail.com>
2022-01-21 00:56:33 +0530

    :rocket: Sample input added

---
+++ src/Sample.js
@@ -0,0 +1,428 @@
+let text = `1
+	
+
+General (Semester)
+	
+
+CSE1901 - Technical Answers for Real World Problems (TARP)
+
+( Embedded Theory )
+	
+
+1 0 0 0 1.0
+	University Core 	
+
+Regular
+	
+
+VL2021220503466
+	
+
+TG1 -
+
+SJT505
+	
+
+AMUTHA PRABAKAR M -
+
+SCOPE
+	
+
+04-Dec-2021 14:11
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+2
+	
+
+General (Semester)
+	
+
+CSE1901 - Technical Answers for Real World Problems (TARP)
+
+( Embedded Project )
+	
+
+0 0 0 4 1.0
+	University Core 	
+
+Regular
+	
+
+VL2021220503467
+	
+
+TG1 -
+
+NIL
+	
+
+AMUTHA PRABAKAR M -
+
+SCOPE
+	
+
+04-Dec-2021 14:11
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+3
+	
+
+General (Semester)
+	
+
+CSE3002 - Internet and Web Programming
+
+( Embedded Theory )
+	
+
+2 0 0 0 2.0
+	Programme Core 	
+
+Regular
+	
+
+VL2021220503380
+	
+
+B2 -
+
+SJT224
+	
+
+SRIDHAR RAJ S -
+
+SCOPE
+	
+
+04-Dec-2021 14:09
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+4
+	
+
+General (Semester)
+	
+
+CSE3002 - Internet and Web Programming
+
+( Embedded Lab )
+	
+
+0 0 2 0 1.0
+	Programme Core 	
+
+Regular
+	
+
+VL2021220503387
+	
+
+L27+L28 -
+
+SJT517
+	
+
+SRIDHAR RAJ S -
+
+SCOPE
+	
+
+04-Dec-2021 14:09
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+5
+	
+
+General (Semester)
+	
+
+CSE3002 - Internet and Web Programming
+
+( Embedded Project )
+	
+
+0 0 0 4 1.0
+	Programme Core 	
+
+Regular
+	
+
+VL2021220503388
+	
+
+L27+L28 -
+
+NIL
+	
+
+SRIDHAR RAJ S -
+
+SCOPE
+	
+
+04-Dec-2021 14:09
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+6
+	
+
+General (Semester)
+	
+
+CSE3050 - Data Visualization and Presentation
+
+( Embedded Theory )
+	
+
+3 0 0 0 3.0
+	University Elective 	
+
+Regular
+	
+
+VL2021220502580
+	
+
+C1+TC1 -
+
+MB311
+	
+
+VISHNU SRINIVASA MURTHY Y -
+
+SCOPE
+	
+
+11-Dec-2021 14:11
+	
+
+12-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+7
+	
+
+General (Semester)
+	
+
+CSE3050 - Data Visualization and Presentation
+
+( Embedded Lab )
+	
+
+0 0 2 0 1.0
+	University Elective 	
+
+Regular
+	
+
+VL2021220502599
+	
+
+L43+L44 -
+
+SJT416
+	
+
+VISHNU SRINIVASA MURTHY Y -
+
+SCOPE
+	
+
+11-Dec-2021 14:11
+	
+
+12-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+8
+	
+
+General (Semester)
+	
+
+CSE3502 - Information Security Management
+
+( Embedded Theory )
+	
+
+2 0 0 0 2.0
+	Programme Elective 	
+
+Regular
+	
+
+VL2021220502523
+	
+
+F2 -
+
+SJT422
+	
+
+VIMALA DEVI K -
+
+SCOPE
+	
+
+04-Dec-2021 14:06
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+9
+	
+
+General (Semester)
+	
+
+CSE3502 - Information Security Management
+
+( Embedded Lab )
+	
+
+0 0 2 0 1.0
+	Programme Elective 	
+
+Regular
+	
+
+VL2021220502642
+	
+
+L7+L8 -
+
+SJT418
+	
+
+VIMALA DEVI K -
+
+SCOPE
+	
+
+04-Dec-2021 14:06
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+10
+	
+
+General (Semester)
+	
+
+CSE3502 - Information Security Management
+
+( Embedded Project )
+	
+
+0 0 0 4 1.0
+	Programme Elective 	
+
+Regular
+	
+
+VL2021220502643
+	
+
+L7+L8 -
+
+NIL
+	
+
+VIMALA DEVI K -
+
+SCOPE
+	
+
+04-Dec-2021 14:06
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved
+
+11
+	
+
+General (Semester)
+	
+
+STS3104 - Enhancing Programming Ability
+
+( Soft Skill )
+	
+
+3 0 0 0 1.0
+	University Core (GENERAL Basket) 	
+
+Regular
+	
+
+VL2021220500149
+	
+
+D1+TD1 -
+
+SJT311
+	
+
+FACE (APT) -
+
+SSL
+	
+
+04-Dec-2021 14:03
+	
+
+05-Dec-2021
+- Manual
+	
+
+Registered and Approved`;
